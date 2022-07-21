df6982bf66b8d6fdd5d8e34082dd247c00968df5
maco <mako.eguchi0425@gmail.com>
2022-07-21 22:49:37 +0900

    Add:タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module MukbangAsmrSearch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # タイムゾーンの設定
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
