60751a5de28d0259e0351d49b9f67115e94e53a8
maco <mako.eguchi0425@gmail.com>
2022-07-23 09:34:20 +0900

    Change:Bootstrap等、gem→Webpackを使って管理に変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all';
+import '../src/application.scss'
 
 Rails.start()
 Turbolinks.start()
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者権限を持つユーザーを追加
+User.create!(
+  { email: "mako.eguchi0425@egmail.com",
+    name: "マコ@admin",
+    password: "12240425",
+    password_confirmation: "12240425",
+    role: 1
+  }
+)
