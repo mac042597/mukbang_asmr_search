e0458acdc03086b1915f42b0cd65769783476ef0
maco <mako.eguchi0425@gmail.com>
2022-07-24 21:04:38 +0900

    Add:日本語化対応の設定

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,10 @@ module MukbangAsmrSearch
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
 
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
