e2b8924d2dc5371b75bffc2ea5df61355185743e
maco <mako.eguchi0425@gmail.com>
2022-08-14 22:03:56 +0900

    Add:daisyUIの導入

--- app/javascript/packs/tailwind.config.js
+++ app/javascript/packs/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  content: ["./app/helpers/**/*.rb", "./app/javascript/packs/*.{js,vue}", "./app/views/**/*.{erb,haml,html,slim}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
