a4d7f654a037ebea1db0b9210345618055d09de2
maco <mako.eguchi0425@gmail.com>
2022-08-14 09:51:43 +0900

    Remove:bootstrapの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap/dist/js/bootstrap.min.js')
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/application';
 
