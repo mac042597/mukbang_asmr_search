b1d0dd60b54017f4c4978adead917cadca264740
maco <mako.eguchi0425@gmail.com>
2022-08-15 10:16:34 +0900

    Fix:tailwind.scssの読込み方法の変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,9 @@ require("@rails/activestorage").start()
 require("channels")
 require('./hello_vue.js')
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/application';
-import '../stylesheets/tailwind.scss';
+
 
 Rails.start()
 Turbolinks.start()
