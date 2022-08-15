23227888723bdcb16fed43f93a840d76bcfcd02f
maco <mako.eguchi0425@gmail.com>
2022-08-15 10:18:54 +0900

    Add:設定の追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./app/javascript/packs/tailwind.config.js'), ,
+    require('tailwindcss')('./app/javascript/packs/tailwind.config.js'),
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
