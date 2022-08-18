41f2fa46912fc2ba99556fd73b5762dee847209d
maco <mako.eguchi0425@gmail.com>
2022-08-18 21:17:30 +0900

    Fix:js周りの設定の変更

--- app/javascript/packs/tailwind.config.js
+++ app/javascript/packs/tailwind.config.js
@@ -1,12 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./app/helpers/**/*.rb", "./app/javascript/packs/*.{js,vue}", "./app/views/**/*.{erb,haml,html,slim}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
