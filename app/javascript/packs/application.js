ddbf8b3dc8f65e14683169bab6463d8e384035ab
maco <mako.eguchi0425@gmail.com>
2022-09-05 00:07:24 +0900

    Add:埋込み動画を表示するモーダルの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('./modal.js')
+require('./jquery-modal-video.min.js')
 require('./application_vue.js')
 
 import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/application';
+import '../stylesheets/modal-video.min';
--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -14,5 +14,18 @@
     $('.current_action_nav').fadeOut();
     //クローズボタンをクリックして、フェードインしたものをフェードアウトで消す
     });
+
+  (function () {
+    if ($(".js-modal-video").length) { //クラス名js-modal-videoがあれば以下を実行
+      $(".js-modal-video").modalVideo({
+        channel: "youtube",
+        youtube: {
+          rel: 0, //関連動画の指定
+          autoplay: 0, //自動再生の指定
+          controls: 1, //コントロールさせるかどうかの指定
+        },
+      });
+    }
+  })();
   })
 }
