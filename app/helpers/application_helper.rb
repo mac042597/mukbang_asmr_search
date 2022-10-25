0544f69ada54862eaab6371ac8c44c4fd6b4b30c
maco <mako.eguchi0425@gmail.com>
2022-10-25 23:52:22 +0900

    Fix:OGPの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,30 @@ module ApplicationHelper
   def active_if(path)
     path == controller_path ? 'active' : ''
   end
+
+  def default_meta_tags
+    {
+      site: 'Muksearch',
+      title: '数クリックでモッパン動画が検索できるサービス',
+      reverse: true,
+      separator: '|',
+      description: 'わずか4クリックでモッパン動画の検索ができるサービスです。日本語＆韓国で検索できるから、沢山の動画を手軽に楽しむことができます！',
+      keywords: 'モッパン',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'Muksearch',
+        title: 'わずか4クリックでモッパン動画が検索できるサービスです',
+        description: 'わずか4クリックでモッパン動画の検索ができるサービスです。日本語＆韓国で検索できるから、沢山の動画を手軽に楽しむことができます！', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+    }
+  end
 end
